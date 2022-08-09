f0f62226db0219b82f9582c0bac7694aef799254
sumanthyashu <sumanthyashu@gmail.com>
2022-08-09 16:46:33 +0530

    First commit

---
+++ precache-manifest.77120fcdf7f4851398c2fda85cdb5662.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5bbe69d03b16da4bfd95cef0b771b18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b6ab3c2163dedc5cb29",
+    "url": "/static/css/main.864708ff.chunk.css"
+  },
+  {
+    "revision": "44711fe16ecebbf4e373",
+    "url": "/static/js/2.aeb6486a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.aeb6486a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b6ab3c2163dedc5cb29",
+    "url": "/static/js/main.11dec649.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "7ce08319d0898e63e893ad90140b01ac",
+    "url": "/static/media/SBV-Logo.7ce08319.png"
+  },
+  {
+    "revision": "46494d973c7f514e0708401935f28ad7",
+    "url": "/static/media/aboutUsImg.46494d97.jpg"
+  },
+  {
+    "revision": "13315e6e5e080a9ea21b9735a2f68053",
+    "url": "/static/media/homeImg.13315e6e.png"
+  }
+]);
